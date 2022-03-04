8b2800d5b1cff87545275b3731f6c45c75b163cb
Albina MV <alyamarvlad@gmail.com>
2022-03-04 17:27:32 +1000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1010;
+        int rubPerMile = 20;
+
+        int milesAmount = ticketPrice / rubPerMile;
+        System.out.println("Начислено миль: " + milesAmount);
+    }
+}
